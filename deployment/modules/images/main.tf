32e87671cecb2946946a900e7e6a2a76b854be3d
lderdzyan <lderdzyan@vtgsoftware.com>
2026-04-10 16:33:29 +0400

    Bucket For Images

--- deployment/modules/images/main.tf
+++ deployment/modules/images/main.tf
@@ -6,14 +6,7 @@ resource "aws_s3_bucket" "bucket" {
     Environment = var.environment
   }
 }
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.project_name}-${var.environment}-images"
 
-  tags = {
-    Name        = "${var.project_name}-images"
-    Environment = var.environment
-  }
-}
 resource "aws_iam_policy" "s3_access" {
   name = "${var.project_name}-${var.environment}-s3-access"
 
