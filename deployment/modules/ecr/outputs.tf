c4481854293e23427dc254c196e907dbdd51fb92
lderdzyan <lderdzyan@vtgsoftware.com>
2026-04-10 15:52:06 +0400

    Resource name correction

--- deployment/modules/ecr/outputs.tf
+++ deployment/modules/ecr/outputs.tf
@@ -1,6 +1,6 @@
 output "repository_urls" {
   value = {
-    for repo, r in aws_ecr_repository.repos :
+    for repo, r in aws_ecr_repository.ecr_repo :
     repo => r.repository_url
   }
 }
--- deployment/modules/eks/outputs.tf
+++ deployment/modules/eks/outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  value = aws_eks_cluster.cluster.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  value = aws_eks_cluster.cluster.endpoint
 }
 
 output "cluster_ca" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
