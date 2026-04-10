62d3b2f0467e7b9c102e179d153a8120bfc5e74c
lderdzyan <lderdzyan@vtgsoftware.com>
2026-04-10 15:55:42 +0400

    AZ update

--- deployment/main.tf
+++ deployment/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   project_name = var.project_name
   environment  = var.environment
 
-  azs = ["eu-central-1a", "eu-central-1b"]
+  azs = ["eu-north-1a", "eu-north-1b"]
 
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
