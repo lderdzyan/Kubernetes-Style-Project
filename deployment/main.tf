d665354691a6967d3ad3a345f439044c3ed933e3
lderdzyan <lderdzyan@vtgsoftware.com>
2026-04-13 14:13:03 +0400

    Zone Update

--- deployment/main.tf
+++ deployment/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   project_name = var.project_name
   environment  = var.environment
 
-  azs = ["${var.aws_region}-1a", "${var.aws_region}-1b"]
+  azs = ["${var.aws_region}a", "${var.aws_region}b"]
 
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
